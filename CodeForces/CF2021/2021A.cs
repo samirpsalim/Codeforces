91a460fa4d7ca065812a0b16c371db5cf4763e9e
Samir P Salim <samirpsalim.git@gmail.com>
2025-10-03 00:31:23 +0530

    Solved 2021A in codeforces.

---
+++ CodeForces/CF2021/2021A.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2021
+{
+    internal class _2021A
+    {
+        private static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                Array.Sort(a);
+
+                for (int j = 1; j < n; j++)
+                {
+                    a[j] = (a[j] + a[j-1]) / 2;
+                }
+
+                Console.WriteLine(a[n-1]);
+            }
+
+        }
+    }
+}
