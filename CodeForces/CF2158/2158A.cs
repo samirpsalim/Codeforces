d98054be112403a81da5f8dda9a31e96de394fe7
Samir P Salim <samirpsalim.git@gmail.com>
2025-12-19 22:42:52 +0530

    Live solutions from Nov 29, 2025

---
+++ CodeForces/CF2158/2158A.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2158
+{
+    internal class _2158A
+    {
+        public static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for(int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var cards = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                Console.WriteLine(Math.Min(n, cards[1] + cards[0]/2));
+                
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2158/2158B.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2158
+{
+    internal class _2158B
+    {
+        static void Solve()
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var countdict = CountOccurences(a);
+
+                Console.WriteLine(Process(countdict, n));
+
+            }
+        }
+
+        private static int Process(Dictionary<int, int> countdict, int n)
+        {
+            var evenbuff = 0;
+            var oddcount = 0;
+            var evencount = 0;
+            var basecount = 0;
+
+            foreach(var i in countdict)
+            {
+                if (i.Value % 2 != 0)
+                {
+                    oddcount += i.Value;
+                    basecount += 1;
+                }
+                else
+                {
+                    evencount++;
+                    evenbuff+=i.Value-2;
+                    basecount += 2;
+                }
+            }
+
+            if(oddcount + evencount >=n)
+            {
+                return basecount;
+            }
+            var target =  n - oddcount - evencount;
+
+            if (target % 2 == 1)
+            {
+                if (oddcount>0)
+                {
+                    target++;
+                }
+                else
+                {
+                    basecount -= 2;
+                    target--;
+                }
+            }
+            if(target<=evenbuff) 
+            { 
+                return basecount;
+            }
+            if (oddcount>0 && (n - oddcount - evencount) % 2 == 1)
+            {
+                target--;
+            }
+            return basecount - 2* (target-evenbuff);
+        }
+
+        private static Dictionary<int,int> CountOccurences(int[] a)
+        {
+            var dict = new Dictionary<int,int>();
+            for (int i = 0; i < a.Length; ++i)
+            {
+                if (dict.ContainsKey(a[i])) dict[a[i]]++;
+                else dict[a[i]] = 1;
+            }
+            return dict;
+        }
+    }
+}
