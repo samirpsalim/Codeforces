4428880e07d5a0f43c323cb58fa4743eb1d43fe4
Samir P Salim <samirpsalim.git@gmail.com>
2025-12-19 23:12:40 +0530

    Live submissions on Dec 19, 2025

---
+++ CodeForces/CF2180/2180A.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2180
+{
+    internal class _2180A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++) 
+            {
+                var inputs = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var l = inputs[0];
+                var a = inputs[1];
+                var b = inputs[2];
+
+                var gcd = GCD(l, b);
+
+                Console.WriteLine((l - 1) - ((l - 1 - a) % gcd));
+            }
+        }
+
+        private static int GCD(int l, int b)
+        {
+            if (l < b) return GCD(b, l);
+
+            if (l % b == 0) return b;
+
+            return GCD(b, l%b);
+
+        }
+    }
+}
---
+++ CodeForces/CF2180/2180B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2180
+{
+    internal class _2180B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ');
+
+                var ans = "";
+
+                foreach(var s in a)
+                {
+                    ans = (ans+s).CompareTo(s+ans)<0 ? ans+s : s+ans;
+                }
+
+                Console.WriteLine(ans);
+
+            }
+        }
+    }
+}
