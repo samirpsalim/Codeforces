8de0775f2e6f85d06314b95e05aa0123137a7d45
Samir P Salim <samirpsalim.git@gmail.com>
2025-12-20 22:48:45 +0530

    Upsolved problems from Dec 19's contest

---
+++ CodeForces/CF2180/2180C.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2180
+{
+    internal class _2180C
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; ++i)
+            {
+                var nums = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                Console.WriteLine(Process(nums[0], nums[1]));
+            }
+        }
+
+        private static string Process(int n, int k)
+        {
+            var ans = new StringBuilder();
+            if(k%2 == 1)
+            {
+                for (int i = 0; i < k; ++i)
+                {
+                    ans.Append(n);
+                    ans.Append(" ");
+                }
+                return ans.ToString();
+            }
+
+            var binstr = GetBinaryString(n, out var bitcount);
+
+            for (int i = 0; i < k - bitcount; ++i)
+            {
+                ans.Append(n);
+                ans.Append(" ");
+            }
+
+            var onesSeen = 0;
+            int numlen = Math.Min(bitcount, k);
+            var nums = new int[numlen];
+            foreach(var x in binstr)
+            {
+                if (x == '1')
+                {
+                    for(int i =0; i<numlen;++i)
+                    {
+                        nums[i] = 2*nums[i] + (i==onesSeen%numlen ? 0 : 1);
+                    }
+                    onesSeen++;
+                }
+                else
+                {
+                    var end = onesSeen>=numlen?
+                        2*(numlen/2) :onesSeen%2==0? onesSeen : onesSeen-1;
+                    for (int i = 0; i < end; ++i)
+                    {
+                        nums[i] = 2 * nums[i] + 1;
+                    }
+                    for (int i = end; i < numlen; ++i)
+                    {
+                        nums[i] = 2 * nums[i];
+                    }
+                }
+
+            }
+
+            ans.Append(string.Join(" ", nums));
+            return ans.ToString();
+        }
+
+        private static string GetBinaryString(int n, out int truecount)
+        {
+            truecount = 0;
+            var st = new StringBuilder();
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    st.Insert(0, '1');
+                    truecount++;
+                }
+                else st.Insert(0, '0');
+                n >>= 1;
+            }
+            return st.ToString();
+        }
+    }
+}
---
+++ CodeForces/CF2180/2180D.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2180
+{
+    internal class _2180D
+    {
+        static void Main(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+            int prev;
+            int cur;
+            int start;
+            int finish;
+
+            var parsing = 0;
+            var curnum = 0;
+            var curpos = 0;
+
+            for (int i = 0; i < t; ++i)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var x = Console.ReadLine()!;
+
+                if (n == 1)
+                {
+                    Console.WriteLine(0);
+                    continue;
+                }
+
+                if (n == 2)
+                {
+                    Console.WriteLine(1);
+                    continue;
+                }
+
+                parsing = 0;
+
+                curnum = 0;
+                curpos = 0;
+                cur = 0;
+                prev = 0;
+
+                while (curnum < 2)
+                {
+                    if (x[curpos] == ' ')
+                    {
+                        curnum++;
+                        if(curnum ==1) prev = parsing;
+                        if(curnum ==2) cur = parsing;
+                        parsing = 0;
+                    }
+                    else
+                    {
+                        parsing = parsing * 10 + (x[curpos]-48);
+                    }
+                    curpos++;
+                }
+
+                var count = 1;
+                start = cur;
+                finish =2 * cur - prev;
+
+                for (int j = 2; j < n; ++j)
+                {
+                    prev = cur;
+                    cur = 0;
+                    while(x[curpos] != ' ')
+                    {
+                        cur = cur * 10 + (x[curpos]-48);
+                        curpos++;
+                        if(curpos == x.Length) break;
+                    }
+                    curpos++;
+
+                    if (cur <= start)
+                    {
+                        start = cur;
+                        finish = 2 * cur - prev;
+                        continue;
+                    }
+                    count++;
+                    if (cur >= finish)
+                    {
+                        var temp = start;
+                        start = 2 * cur - finish;
+                        finish =2 * cur - temp;
+                        continue;
+                    }
+                    finish = 2 * cur - start;
+                    start =cur;
+                }
+                Console.WriteLine(count);
+
+            }
+        }
+    }
+}
