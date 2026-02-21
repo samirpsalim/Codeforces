91751f89f80a32606f1fec785e6fb902089b5e38
Samir P Salim <samirpsalim.git@gmail.com>
2026-02-22 00:52:10 +0530

    Live submissions from Goodbye 2025 contest

---
+++ CodeForces/CF2178/2178A.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.CodeForces.CF2178
+{
+    internal class _2178A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var s = Console.ReadLine();
+
+                var Ycount = 0;
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    if (s[j] == 'Y')
+                    {
+                        Ycount++;
+                        if(Ycount>1) break;
+                    }
+                }
+
+                Console.WriteLine(Ycount > 1 ? "NO" : "YES");
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2178/2178B.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode.CodeForces.CF2178
+{
+    internal class _2178B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var s = Console.ReadLine()!;
+
+                var ans = 0;
+
+                if (s[0] == 'u') ans++;
+
+                if (s[s.Length - 1] == 'u') ans++;
+
+                var sindices = new int[2];
+
+                for(int j=1; j< s.Length -1;++j)
+                {
+                    if (s[j] == 'u') sindices[j%2]++;
+                    else
+                    {
+                        ans += sindices.Min();
+                        sindices[0] = 0; sindices[1]=0;
+                    }
+                }
+
+                Console.WriteLine(sindices.Min() +ans);
+
+            }
+        }
+    }
+}
