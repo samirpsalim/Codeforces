41ce3c1d95bd5387546920219e6715632ae761a8
Samir P Salim <samirpsalim.git@gmail.com>
2026-02-27 20:05:35 +0530

    Live submission from contest on Feb 26 2025

---
+++ CodeForces/CF2205/2205A.cs
@@ -0,0 +1,24 @@
+﻿namespace Leetcode.CodeForces.CF2205
+{
+    internal class _2205A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for(int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var p = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+
+                var index = p.IndexOf(n);
+
+                var temp = p[index];
+                p[index] = p[0];
+                p[0] = temp;
+
+                Console.WriteLine(string.Join(" ", p));
+            }
+        }
+    }
+}
