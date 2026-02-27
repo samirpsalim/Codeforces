18030c1428442508c3d2cb150e0a850c8ca66f08
Samir P Salim <samirpsalim.git@gmail.com>
2026-02-27 22:33:39 +0530

    Live submissions from contest on Feb 27, 2026

---
+++ CodeForces/CF2200/2200A.cs
@@ -0,0 +1,17 @@
+﻿namespace Leetcode.CodeForces.CF2200
+{
+    internal class _2200A
+    {
+        static void Solve(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++) {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+                var max = a.Max();
+                Console.WriteLine(a.Count(a => a==max));
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2200/2200B.cs
@@ -0,0 +1,33 @@
+﻿
+
+namespace Leetcode.CodeForces.CF2200
+{
+    internal class _2200B
+    {
+        static void Solve(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+                Console.WriteLine(GetMinimumElementLeft(a.AsSpan(),n));
+            }
+        }
+
+        private static int GetMinimumElementLeft(Span<int> span, int size)
+        {
+            if (size == 1)
+                return 1;
+
+            for(int i = 1; i < size; i++)
+            {
+                if (span[i] < span[i - 1])
+                    return 1;
+            }
+
+            return size;
+        }
+    }
+}
