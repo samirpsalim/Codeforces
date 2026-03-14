2440400a7b69cf3d626bbd33c8ee8debe683d7a9
Samir P Salim <samirpsalim.git@gmail.com>
2026-03-14 22:14:51 +0530

    Live submissions on March 14, 2026

---
+++ CodeForces/CF2208/2208A.cs
@@ -0,0 +1,46 @@
+﻿namespace Leetcode.CodeForces.CF2208
+{
+    internal class _2208A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++) { 
+                var n = int.Parse(Console.ReadLine()!);
+
+                if(n==1)
+                {
+                    Console.ReadLine();
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    var a = new List<List<int>>();
+
+                    for (int j = 0; j < n; j++)
+                    {
+                        a.Add(Console.ReadLine()!.Split(' ').Select(int.Parse).ToList());
+                    }
+
+                    var elemdict = new Dictionary<int, int>();
+                    var possible = true;
+
+                    foreach (var row in a)
+                    {
+                        foreach (var val in row)
+                        {
+                            if (elemdict.ContainsKey(val)) elemdict[val]++;
+                            else elemdict[val] = 1;
+
+                            if (elemdict[val] > (n - 1) * n) possible = false;
+                        }
+                    }
+
+                    Console.WriteLine(possible ? "YES" : "NO");
+                }
+
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2208/2208C.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode.CodeForces.CF2208
+{
+    record ProblemTask(float c,float p);
+
+    internal class _2208C
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var tasks = new ProblemTask[n];
+
+                for (int j = 0; j < n; j++)
+                {
+                    var input =Console.ReadLine()!.Split(' ');
+                    tasks[n-1-j] =new ProblemTask(float.Parse(input[0]), float.Parse(input[1]));
+                }
+
+                float cj = tasks[0].c;
+                foreach (var task in tasks.Skip(1))
+                {
+                    if(100*task.c>=task.p*cj)
+                    {
+                        cj = task.c + (1 - (task.p / 100)) * cj;
+                    }
+                }
+                Console.WriteLine(cj);
+            }
+        }
+    }
+}
