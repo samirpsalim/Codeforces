27f96423ecf55cbe48de07aa80fbc3a9e651c867
Samir P Salim <samirpsalim.git@gmail.com>
2026-04-07 22:25:55 +0530

    Live submissions from contest on April 7 2026

---
+++ CodeForces/CF2217/2217A.cs
@@ -0,0 +1,22 @@
+﻿namespace Leetcode.CodeForces.CF2217
+{
+    internal class _2217A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var inputs = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+                var n = inputs[0];
+                var k = inputs[1];
+
+                var asum = Console.ReadLine()!.Split(' ').Select(int.Parse).Sum();
+
+                Console.WriteLine(n * k % 2 == 0 || asum % 2 == 1 ? "YES" : "NO");
+
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2217/2217B.cs
@@ -0,0 +1,35 @@
+﻿namespace Leetcode.CodeForces.CF2217
+{
+    internal class _2217B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!.Split(' ')[0]);
+
+                var a = Console.ReadLine()!.Split(' ').Select(x => x == "1").ToArray();
+                var p = int.Parse(Console.ReadLine()!);
+
+                var flips = 0;
+                var maxflip = 0;
+
+                for(int j =0; j<p-1;++j)
+                {
+                    if (a[j] != a[j+1]) flips++;
+                }
+                maxflip = flips;
+                flips = 0;
+                for (int j = p; j < n; ++j)
+                {
+                    if (a[j] != a[j - 1]) flips++;
+                }
+                maxflip =  flips <= maxflip ? maxflip : flips;
+
+                Console.WriteLine(maxflip % 2 == 0 ? maxflip : maxflip + 1);
+
+            }
+        }
+    }
+}
