42d4e889314872279a00e71af8697131e98b38b4
Samir P Salim <samirpsalim.git@gmail.com>
2026-04-14 23:42:09 +0530

    Live submissions from contest on april 13

---
+++ CodeForces/CF2220/2220A.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode.CodeForces.CF2220
+{
+    class _2220A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for(int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                if(a.Distinct().Count()!=n)
+                {
+                    Console.WriteLine(-1);
+                }
+                else
+                {
+                    Array.Sort(a);
+					Console.WriteLine(string.Join(' ',a.Reverse()));
+				}
+			}
+        }
+    }
+}
---
+++ CodeForces/CF2220/2220B.cs
@@ -0,0 +1,34 @@
+﻿namespace Leetcode.CodeForces.CF2220
+{
+    internal class _2220B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+            int n, m;
+
+            for (int i = 0; i < t; i++)
+            {
+                var lens = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                (n,m) = (lens[0],lens[1]);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var currep = 1;
+                for(int j=1; j < n; j++)
+                {
+                    if (a[j] == a[j - 1])
+                    {
+                        currep++;
+                        if (currep == m) break;
+                    }
+                    else
+                    {
+                        currep = 1;
+                    }
+                }
+                Console.WriteLine(currep == m ? "NO" : "YES");
+            }
+        }
+    }
+}
