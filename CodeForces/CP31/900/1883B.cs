44e7c0b6c60be7546ff28cc1f9fcb9c456313a6b
Samir P Salim <samirpsalim.git@gmail.com>
2026-04-22 11:37:11 +0530

    CP31 sheet - 900 rated - commit 1

---
+++ CodeForces/CP31/900/1883B.cs
@@ -0,0 +1,51 @@
+﻿namespace Leetcode.CodeForces.CP31._900
+{
+    internal class _1883B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            var chardict = new Dictionary<char, int>(26);
+
+            for (int i = 0; i < t; i++)
+            {
+                var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (n, k) = (arr[0], arr[1]);
+
+                var s = Console.ReadLine()!;
+
+                foreach (char c in s)
+                {
+                    if (chardict.ContainsKey(c))
+                    {
+                        chardict[c]++;
+                    }
+                    else
+                    {
+                        chardict.Add(c, 1);
+                    }
+                }
+
+                var equalizingValue = chardict.Values.Sum(v=> v%2);
+                if(equalizingValue % 2 != k % 2)
+                {
+                    if (equalizingValue > 0) equalizingValue--;
+                    else equalizingValue = 1;
+                }
+
+                if (equalizingValue>k || equalizingValue % 2 != k % 2)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                }
+
+                chardict.Clear();
+            }
+        }
+    }
+}
---
+++ CodeForces/CP31/900/1904A.cs
@@ -0,0 +1,39 @@
+﻿namespace Leetcode.CodeForces.CP31._900
+{
+    internal class _1904A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (a, b) = (arr[0], arr[1]);
+
+                arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (xk, yk) = (arr[0], arr[1]);
+
+                arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (xq, yq) = (arr[0], arr[1]);
+
+                HashSet<(int, int)> ks = new()
+                {
+                    (xk+a, yk+b), (xk-a, yk+b), (xk+a, yk-b), (xk-a, yk-b),
+                    (xk+b, yk+a), (xk-b, yk+a), (xk+b, yk-a), (xk-b, yk-a)
+                };
+
+                HashSet<(int, int)> qs = new()
+                {
+                    (xq+a, yq+b), (xq-a, yq+b), (xq+a, yq-b), (xq-a, yq-b),
+                    (xq+b, yq+a), (xq-b, yq+a), (xq+b, yq-a), (xq-b, yq-a)
+                };
+
+                Console.WriteLine(ks.Count(p => qs.Contains(p)));
+            }
+        }
+    }
+}
