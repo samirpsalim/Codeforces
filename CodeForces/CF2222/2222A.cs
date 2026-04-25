70811a2bc9f7269d96ad8b0e07bd893f7c8a0b2a
Samir P Salim <samirpsalim.git@gmail.com>
2026-04-25 22:41:54 +0530

    Live submissions from contest on 25 April 2026

---
+++ CodeForces/CF2222/2222A.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.CodeForces.CF2222
+{
+    internal class _2222A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var ans = Console.ReadLine()!.Split(' ').Select(int.Parse).Any(ai => ai==100);
+
+                Console.WriteLine(ans? "YES" : "NO");
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2222/2222B.cs
@@ -0,0 +1,79 @@
+﻿namespace Leetcode.CodeForces.CF2222
+{
+    internal class _2222B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (n,m) = (arr[0],arr[1]);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var oddidxcount = Console.ReadLine()!.Split(' ').Select(int.Parse).Count(xi => xi%2==1);
+                var evenidxcount = m-oddidxcount;
+
+                var oddidxarr = new int[(n+1)/2];
+                var evenidxarr = new int[n/2];
+
+                for(int j =0; j < n; j++)
+                {
+                    if(j%2 == 0)
+                    {
+                        oddidxarr[j/2] = a[j];
+                    }
+                    else
+                    {
+                        evenidxarr[j/2] = a[j];
+                    }
+                }
+
+                Array.Sort(oddidxarr);
+                Array.Sort(evenidxarr);
+
+                var sum = 0L;
+
+                var last = (n + 1) / 2 - (oddidxcount > 0 ? 1 : 0);
+                for (int j = 0; j < last; j++)
+                {
+                    if (oddidxcount + j < (n + 1) / 2)
+                    {
+                        sum += oddidxarr[j];
+                    }
+                    else if (oddidxarr[j] < 0)
+                    {
+                        sum += oddidxarr[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                last = n / 2 - (evenidxcount > 0 ? 1 : 0);
+                for (int j = 0; j < last; j++)
+                {
+                    if (evenidxcount + j < n / 2)
+                    {
+                        sum += evenidxarr[j];
+                    }
+                    else if (evenidxarr[j] < 0)
+                    {
+                        sum += evenidxarr[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine(sum);
+
+            }
+        }
+    }
+}
