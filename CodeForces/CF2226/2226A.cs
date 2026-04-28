8135185fc5386e4098bb1c5f3c0a4e346c04e492
Samir P Salim <samirpsalim.git@gmail.com>
2026-04-28 22:21:08 +0530

    Live submissions from contest of 28 April 2026

---
+++ CodeForces/CF2226/2226A.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode.CodeForces.CF2226
+{
+    internal class _2226A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+                var ans = 0;
+                var isOnOne = false;
+
+                for(int j = 0; j < n; j++)
+                {
+                    if (a[j]==1)
+                    {
+                        isOnOne=true;
+                    }
+                    else
+                    {
+                        ans = (ans + a[j]) % 676767677;
+                        isOnOne = false;
+                    }
+                }
+
+                if(isOnOne)
+                    ans = (ans + 1) % 676767677;
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2226/2226B.cs
@@ -0,0 +1,31 @@
+﻿namespace Leetcode.CodeForces.CF2226
+{
+    internal class _2226B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var ans = 0;
+
+                for(int j=0; j<n-1; j++)
+                {
+                    var diff = a[j] > a[j + 1] ? a[j] - a[j + 1] : a[j+1] - a[j];
+
+                    if (a[j]%diff==0 && a[j+1]%diff==0)
+                    {
+                        ans++;
+                    }
+                }
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
