fb4047ef74781fd329e9acd216943c9487efb946
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-04 09:58:46 +0530

    CP31 sheet -1100 rated - commit 4 - edit

--- CodeForces/CP31/1100/1869B.cs
+++ CodeForces/CP31/1100/1869B.cs
@@ -2,7 +2,7 @@
 {
     internal class _1869B
     {
-        static void Main(string[] args)
+        static void Solve(string[] args)
         {
             var t = int.Parse(Console.ReadLine()!);
 
