24e011d047a95fbf14d9929cad4bf80aae5ffd67
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-16 00:11:31 +0530

    CP31 sheet -1100 rated - commit 16

---
+++ CodeForces/CP31/1100/1511C.cs
@@ -0,0 +1,42 @@
+﻿namespace Leetcode.CodeForces.CP31._1100
+{
+    internal class _1511C
+    {
+        static void Solve(string[] args)
+        {
+            var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+            var (n,q) = (arr[0], arr[1]);
+
+            var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+            var t = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+            var coldict = new Dictionary<int, int>(50);
+
+            for(int j=0; j<n; j++)
+            {
+                if(!coldict.ContainsKey(a[j]))
+                {
+                    coldict[a[j]] = j+1;
+
+                    if(coldict.Count==50) break;
+                }
+            }
+
+            foreach(var tj in t)
+            {
+                var caseans = coldict[tj];
+                foreach(var kj in coldict.Keys)
+                {
+                    if(coldict[kj] < caseans)
+                    {
+                        coldict[kj]++;
+                    }
+                }
+                Console.Write($"{caseans} ");
+                coldict[tj] = 1;
+            }
+        }
+    }
+}
---
+++ CodeForces/CP31/1100/1610B.cs
@@ -0,0 +1,120 @@
+﻿namespace Leetcode.CodeForces.CP31._1100
+{
+    internal class _1610B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var start = 0;
+                var end = n - 1;
+
+                int possible1, possible2;
+
+                while (start < end)
+                {
+                    if (a[start] != a[end])
+                    {
+                        break;
+                    }
+                    start++;
+                    end--;
+                }
+
+                if(start >= end)
+                {
+                    Console.WriteLine("YES");
+                    continue;
+                }
+
+                possible1 = a[start];
+                possible2 = a[end];
+
+                start=0;
+                end = n - 1;
+                while (a[start] ==possible1)
+                {
+                    start++;
+                    if (start == n) break;
+                }
+                while (a[end] ==possible1)
+                {
+                    end--;
+                    if(end <0) break;
+                }
+
+                var flag = false;
+
+                while (start < end)
+                {
+                    if (a[start] != a[end])
+                    {
+                        flag = true;
+                        break;
+                    }
+                    start++;
+                    end--;
+                    while (a[start] == possible1)
+                    {
+                        start++;
+                        if (start == n) break;
+                    }
+                    while (a[end] == possible1)
+                    {
+                        end--;
+                        if (end < 0) break;
+                    }
+                }
+
+                if (!flag)
+                {
+                    Console.WriteLine("YES");
+                    continue;
+                }
+
+                start = 0;
+                end = n - 1;
+                while (a[start] == possible2)
+                {
+                    start++;
+                    if (start == n) break;
+                }
+                while (a[end] == possible2)
+                {
+                    end--;
+                    if (end < 0) break;
+                }
+
+                flag = false;
+
+                while (start < end)
+                {
+                    if (a[start] != a[end])
+                    {
+                        flag = true;
+                        break;
+                    }
+                    start++;
+                    end--;
+                    while (a[start] == possible2)
+                    {
+                        start++;
+                        if (start == n) break;
+                    }
+                    while (a[end] == possible2)
+                    {
+                        end--;
+                        if (end < 0) break;
+                    }
+                }
+
+                Console.WriteLine(flag? "NO" : "YES");
+            }
+        }
+    }
+}
