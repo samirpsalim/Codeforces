fd680fc4597f06c48904186c5b7fe75f13460ffa
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-16 13:53:33 +0530

    CP31 sheet - 1200 rated - commit 1

---
+++ CodeForces/CP31/1200/1703E.cs
@@ -0,0 +1,36 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1703E
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                int[][] a = new int[n][];
+
+                for(int j = 0; j < n; j++)
+                {
+                    a[j] = Console.ReadLine()!.Select(ai=> ai-48).ToArray();
+                }
+
+                var ans = 0;
+
+                for(int j = 0;j < n-1-j; j++)
+                {
+                    for (int k = 0; k <= n - 1 - k; k++)
+                    {
+                        var ones = a[j][k] + a[k][n-1-j] + a[n-1-j][n-1-k] + a[n-1-k][j];
+
+                        ans += ones <= 2 ? ones : 4 - ones;
+                    }
+                }
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
---
+++ CodeForces/CP31/1200/1914D.cs
@@ -0,0 +1,49 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal record Day(int date, int friends);
+
+    internal class _1914D
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).Select((ai,idx) => new Day(idx,ai))
+                        .OrderByDescending(ai=> ai.friends).Take(3).ToArray();
+                var b = Console.ReadLine()!.Split(' ').Select(int.Parse).Select((ai, idx) => new Day(idx, ai))
+                        .OrderByDescending(ai => ai.friends).Take(3).ToArray();
+                var c = Console.ReadLine()!.Split(' ').Select(int.Parse).Select((ai, idx) => new Day(idx, ai))
+                        .OrderByDescending(ai => ai.friends).Take(3).ToArray();
+
+                var ans = 0;
+
+                foreach(var ai in a)
+                {
+                    foreach(var bi in b)
+                    {
+                        if (ai.date == bi.date)
+                        {
+                            continue;
+                        }
+                        foreach(var ci in c)
+                        {
+                            if (ai.date == ci.date || bi.date==ci.date)
+                            {
+                                continue;
+                            }
+                            ans = Math.Max(ans, ai.friends+bi.friends+ci.friends);
+
+                        }
+
+                    }
+                }
+
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
