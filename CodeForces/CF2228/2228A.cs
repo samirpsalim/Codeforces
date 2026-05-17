24391d8d7640a529bdaf34ef6297f2db8a9cb28e
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-17 15:48:02 +0530

    Live submissions from contest on 16th May 2026

---
+++ CodeForces/CF2228/2228A.cs
@@ -0,0 +1,31 @@
+﻿namespace Leetcode.CodeForces.CF2228
+{
+    internal class _2228A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var w = Console.ReadLine()!.Split(' ').Select(int.Parse).Where(ai=> ai!=0).ToArray();
+
+                var ans = n-w.Length;
+
+
+                var ones = w.Count(wi => wi == 1);
+                var twos = w.Length -ones;
+
+                var min = Math.Min(ones, twos);
+
+                ones -= min;
+                twos -= min;
+
+                Console.WriteLine(ans+min+ (ones+twos)/3);
+
+            }
+        }
+    }
+}
---
+++ CodeForces/CF2228/2228B.cs
@@ -0,0 +1,25 @@
+﻿namespace Leetcode.CodeForces.CF2228
+{
+    internal class _2228B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                if (a[0]==3 || a[0]==2)
+                {
+                    Console.WriteLine(1);
+                    continue;
+                }
+
+                var dist = Math.Abs(a[1] - a[2]);
+                dist = Math.Min(dist, a[0]-dist);
+                Console.WriteLine(dist + a[3]);
+            }
+        }
+    }
+}
