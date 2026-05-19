43216aef71ef07c01ea03c1749d84296780c4060
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-19 23:16:15 +0530

    CP31 sheet - 1200 rated - commit 4

---
+++ CodeForces/CP31/1200/1808B.cs
@@ -0,0 +1,48 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1808B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (n, m) = (arr[0], arr[1]);
+
+                var c = new int[m][];
+
+                for(int j =0; j < m; j++)
+                {
+                    c[j] = new int[n];
+                }
+
+                for(int j = 0; j < n; j++)
+                {
+                    var cj = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                    for(int k = 0; k < m; k++)
+                    {
+                        c[k][j] = cj[k];
+                    }
+                }
+
+                var ans = 0L;
+
+                foreach(var ci in c)
+                {
+                    Array.Sort(ci);
+
+                    for(int j =1; j < n; j++)
+                    {
+                        ans += (long)j*(n - j) *(ci[j] - ci[j - 1]);
+                    }
+                }
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
---
+++ CodeForces/CP31/1200/1832C.cs
@@ -0,0 +1,49 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal enum Direction
+    {
+        Ascending,
+        Descending,
+        Either
+    }
+
+    internal class _1832C
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var ans = 1;
+
+                var dir = Direction.Either;
+
+                for(int j = 1; j < n; j++)
+                {
+                    if(dir == Direction.Ascending && a[j] < a[j-1])
+                    {
+                        dir = Direction.Descending;
+                        ans++;
+                    }
+                    else if (dir == Direction.Descending && a[j] > a[j - 1])
+                    {
+                        dir = Direction.Ascending;
+                        ans++;
+                    }
+                    else if(dir == Direction.Either && a[j] != a[j-1])
+                    {
+                        ans++;
+                        dir = a[j] > a[j-1] ? Direction.Ascending : Direction.Descending;
+                    }
+                }
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
