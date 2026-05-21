673a2cf10ffde8e404599fb9bb0fc3ef60190583
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-22 00:54:17 +0530

    CP31 sheet - 1200 rated - commit 6

---
+++ CodeForces/CP31/1200/1742E.cs
@@ -0,0 +1,57 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1742E
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (n, q) = (arr[0], arr[1]);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var k = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var maxes = new int[n + 1];
+                var sums = new long[n + 1];
+
+                for(int j=0; j < n; j++)
+                {
+                    maxes[j+1] = Math.Max(maxes[j], a[j]);
+                    sums[j+1] = sums[j] + a[j];
+                }
+
+                var ans = new long[q];
+
+                for(int j=0; j < q; ++j)
+                {
+                    var start = 0;
+                    var end = n;
+                    var mid = start + (end - start)/2;
+                    
+                    while(start < end)
+                    {
+                        if(maxes[mid] > k[j])
+                        {
+                            end = mid - 1;
+                        }
+                        else
+                        {
+                            start = mid;
+                        }
+
+                        mid = start + (end + 1 - start)/2;
+                    }
+
+                    ans[j] = sums[mid];
+                }
+
+                Console.WriteLine(string.Join(" ", ans));
+            }
+        }
+    }
+}
---
+++ CodeForces/CP31/1200/1790D.cs
@@ -0,0 +1,58 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1790D
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).Order().ToArray();
+
+                var ans = 0;
+                var last = 0;
+                var active = 0;
+                var curactive = 0;
+
+                foreach (var ai in a)
+                {
+                    if(last == 0)
+                    {
+                        curactive++;
+                        last = ai;
+                        continue;
+                    }
+
+                    if (ai == last)
+                    {
+                        curactive++;
+                        continue;
+                    }
+
+                    if(ai == last+1)
+                    {
+                        if(curactive < active)
+                        {
+                            ans += active - curactive;
+                        }
+                        active = curactive;
+                        last = ai;
+                        curactive = 1;
+
+                        continue;
+                    }
+
+                    ans += Math.Max(active,curactive);
+                    active = 0;
+                    curactive = 1;
+                    last = ai;
+                }
+
+                Console.WriteLine(ans+Math.Max(curactive, active));
+            }
+        }
+    }
+}
