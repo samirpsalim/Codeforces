c6f2ffe193c1d656000fe31fe03ba0ebc4224570
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-24 15:39:36 +0530

    CP31 sheet - 1200 rated - commit 9

---
+++ CodeForces/CP31/1200/1692E.cs
@@ -0,0 +1,55 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1692E
+    {
+		static void Solve(string[] args)
+		{
+			var t = int.Parse(Console.ReadLine()!);
+
+			for (int i = 0; i < t; i++)
+			{
+				var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+				var (n, s) = (arr[0], arr[1]);
+
+				var a = Console.ReadLine()!.Split(' ').Select(ai => ai == "1").ToArray();
+
+				var posList = new List<int>(n);
+
+				posList.Add(0);
+
+				for (int j = 0; j < n; j++)
+				{
+					if (a[j])
+					{
+						posList.Add(j+1);
+					}
+				}
+
+				var sum = posList.Count-1;
+
+				posList.Add(n + 1);
+
+				if(sum<s)
+				{
+                    Console.WriteLine(-1);
+					continue;
+                }
+
+				if (sum == s)
+				{
+                    Console.WriteLine(0);
+					continue;
+                }
+
+				var maxsize = 0;
+
+				for (int j = 0;j < posList.Count-s-1; j++)
+				{
+					maxsize = Math.Max(maxsize, posList[j + s+1] - posList[j]);
+				}
+
+                Console.WriteLine(n+1-maxsize);
+            }
+		}
+    }
+}
---
+++ CodeForces/CP31/1200/1704C.cs
@@ -0,0 +1,47 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1704C
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (n,m) = (arr[0],arr[1]);
+
+                long ans = m;
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).Order().ToArray();
+
+                var segments = new int[m];
+
+                for(int j=1; j<m; j++)
+                {
+                    segments[j - 1] = a[j] - a[j - 1]-1;
+                }
+
+                segments[m - 1] = n + a[0] - a[m - 1]-1;
+
+                Array.Sort(segments);
+
+                var prevclear = 0;
+
+                for(int j=m-1; j >= 0; j--)
+                {
+                    var cur = Math.Max(0, segments[j]- prevclear);
+
+                    if (cur > 1) cur--;
+                    ans += segments[j] - cur;
+
+                    prevclear += 4;
+                }
+
+                Console.WriteLine(ans);
+
+            }
+        }
+    }
+}
