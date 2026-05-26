ab9d64d6ba6ef3d46ef0df9c59023a8905ebd122
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-26 10:05:09 +0530

    CP31 sheet - 1200 rated - commit 11

---
+++ CodeForces/CP31/1200/1671C.cs
@@ -0,0 +1,38 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1671C
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var (n,x) = (arr[0],arr[1]);
+
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).Order().ToArray();
+
+                var sum = 0L;
+                var ans = 0L;
+
+                for(int j=0; j<n; j++)
+                {
+                    sum += a[j];
+
+                    if(sum>x)
+                    {
+                        break;
+                    }
+
+                    var diff = x-sum;
+                    var diffcount = diff / (j + 1);
+                    ans+= diffcount+1;
+                }
+
+                Console.WriteLine(ans);
+            }
+        }
+    }
+}
