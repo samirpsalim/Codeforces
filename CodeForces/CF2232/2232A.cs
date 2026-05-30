fbe86b9ad5cf527cf5feb64b470bb41fa36351e8
Samir P Salim <samirpsalim.git@gmail.com>
2026-05-30 23:14:15 +0530

    Live submissions from contest on May 30, 2026

---
+++ CodeForces/CF2232/2232A.cs
@@ -0,0 +1,55 @@
+﻿namespace Leetcode.CodeForces.CF2232
+{
+    internal class _2232A
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).Order().ToArray();
+
+                if(n%2 == 0)
+                {
+                    if (a[n / 2] != a[n/2-1])
+                    {
+                        Console.WriteLine(n/2);
+                    }
+                    else
+                    {
+                        var med = a[n / 2];
+                        var less = 0;
+                        var eq = 0;
+
+                        foreach(var ai in a)
+                        {
+                            if (ai < med) less++;
+                            else if (ai == med) eq++;
+                            else break;
+                        }
+
+                        Console.WriteLine(Math.Max(less,n-less-eq));
+                    }
+                }
+                else
+                {
+                    var med = a[n / 2];
+                    var less = 0;
+                    var eq = 0;
+
+                    foreach (var ai in a)
+                    {
+                        if (ai < med) less++;
+                        else if (ai == med) eq++;
+                        else break;
+                    }
+
+                    Console.WriteLine(Math.Max(less, n - less - eq));
+                }
+            }
+
+        }
+    }
+}
---
+++ CodeForces/CF2232/2232B.cs
@@ -0,0 +1,30 @@
+﻿namespace Leetcode.CodeForces.CF2232
+{
+    internal class _2232B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var ans = new int[n];
+
+                ans[0] = a[0];
+                long sum = a[0];
+
+                for (int j = 1; j < n; j++)
+                {
+                    sum+= a[j];
+                    var avg = sum / (j + 1);
+                    ans[j] = (int)Math.Min(ans[j-1], avg);
+                }
+
+                Console.WriteLine(string.Join(' ',ans));
+            }
+        }
+    }
+}
