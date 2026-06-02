3a4fecc3959ba3caac3c14fb02c7313a9eb3b3ad
Samir P Salim <samirpsalim.git@gmail.com>
2026-06-02 10:28:28 +0530

    CP 31 sheet - 1200 rated - commit 18

---
+++ CodeForces/CP31/1200/1433D.cs
@@ -0,0 +1,48 @@
+﻿namespace Leetcode.CodeForces.CP31._1200
+{
+    internal class _1433D
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+                var firstidx = 0;
+                var secindx = -1;
+
+                for(int j =1; j<n; ++j)
+                {
+                    if (a[j]!= a[firstidx])
+                    {
+                        secindx = j;
+                        break;
+                    }
+                }
+
+                if(secindx == -1)
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                    for(int j = 1; j<n; ++j)
+                    {
+                        if(a[j]!= a[firstidx])
+                        {
+                            Console.WriteLine($"{firstidx+1} {j+1}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"{secindx + 1} {j + 1}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
