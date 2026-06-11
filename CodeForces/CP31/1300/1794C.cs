c0227716a6fbca6c01766615e6d6fc8987cd40ae
Samir P Salim <samirpsalim.git@gmail.com>
2026-06-11 09:17:18 +0530

    CP31 sheet- 1300 rated - Q8

---
+++ CodeForces/CP31/1300/1794C.cs
@@ -0,0 +1,42 @@
+﻿namespace Leetcode.CodeForces.CP31._1300
+{
+    internal class _1794C
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+                var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToList();
+
+                var ans = new int[n];
+
+                for (int j = 0; j < n; j++)
+                {
+                    var start = 0;
+                    var end = j;
+                    var mid = (start + end+1) / 2;
+
+                    while (start < end)
+                    {
+                        if (a[j-mid] < mid+1)
+                        {
+                            end = mid-1;
+                        }
+                        else
+                        {
+                            start = mid;
+                        }
+                        mid = (start + end+1) / 2;
+                    }
+
+                    ans[j] = mid+1;
+                }
+
+                Console.WriteLine(string.Join(' ',ans));
+            }
+        }
+    }
+}
