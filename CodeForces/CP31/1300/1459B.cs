015f41a34f3a2e0acebfbf034494e4c1ca320f4e
Samir P Salim <samirpsalim.git@gmail.com>
2026-08-17 22:34:39 +0530

    CP31 sheet - 1300 rated - Q18

---
+++ CodeForces/CP31/1300/1459B.cs
@@ -0,0 +1,19 @@
+﻿namespace Leetcode.CodeForces.CP31._1300
+{
+    internal class _1459B
+    {
+        static void Solve(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine()!);
+
+            var ans = (n % 2) switch
+            {
+                0 => (n+2)*(n+2)/4,
+                1 => (n+3)*((n+2)/2),
+                _=> 0
+            };
+
+            Console.WriteLine(ans);
+        }
+    }
+}
