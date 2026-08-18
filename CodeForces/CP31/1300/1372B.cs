463a6876e301b15f3ef23aac8376c629ac0702a9
Samir P Salim <samirpsalim.git@gmail.com>
2026-08-18 22:52:31 +0530

    CP31 sheet - 1300 rated - Q19

---
+++ CodeForces/CP31/1300/1372B.cs
@@ -0,0 +1,28 @@
+﻿namespace Leetcode.CodeForces.CP31._1300
+{
+    internal class _1372B
+    {
+        static void Solve(string[] args)
+        {
+            var t = int.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                var n = int.Parse(Console.ReadLine()!);
+
+                var div = MinFactor(n);
+
+                Console.WriteLine($"{n/div} {n-(n/div)}");
+            }
+        }
+
+        private static int MinFactor(int n)
+        {
+            for (int i = 2;i*i<=n; i++)
+            {
+                if(n%i==0) return i;
+            }
+            return n;
+        }
+    }
+}
