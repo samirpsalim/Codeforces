432137166912f8cfbdf4de867166c8b611e8dec2
Samir P Salim <samirpsalim.git@gmail.com>
2026-08-28 21:56:03 +0530

    CP31 sheet - 1300 rated - Q27, Q28

---
+++ CodeForces/CP31/1300/1077C.cs
@@ -0,0 +1,52 @@
+﻿namespace Leetcode.CodeForces.CP31._1300
+{
+    internal class _1077C
+    {
+        static void Solve(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine()!);
+
+            var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+            var countdict = new Dictionary<long, int>();
+            var sum = 0L;
+
+            for (int i = 0; i < n; i++)
+            {
+                if(countdict.ContainsKey(a[i]))
+                {
+                    countdict[a[i]]++;
+                }
+                else
+                {
+                    countdict[a[i]] = 1;
+                }
+                sum += a[i];
+            }   
+            
+            var ans = new List<int>();
+        
+            for (int i = 0;i < n; i++)
+            {
+                if ((sum - a[i])%2!=0)
+                {
+                    continue;
+                }
+                long half = (sum - a[i]) / 2;
+
+                if(half == a[i])
+                {
+                    if (countdict[half]>1)
+                        ans.Add(i+1);
+                }
+                else if (countdict.ContainsKey(half))
+                {
+                    ans.Add(i+1);
+                }
+            }
+
+            Console.WriteLine(ans.Count);
+            Console.WriteLine(string.Join(' ',ans));
+        }
+    }
+}
---
+++ CodeForces/CP31/1300/1119B.cs
@@ -0,0 +1,37 @@
+﻿namespace Leetcode.CodeForces.CP31._1300
+{
+    internal class _1119B
+    {
+        static void Solve(string[] args)
+        {
+            var arr = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+            var (n, h) = (arr[0], arr[1]);
+
+            var a = Console.ReadLine()!.Split(' ').Select(int.Parse).ToArray();
+
+            var ans = 0;
+            var maxh = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (i % 2 == 0) maxh += a[i];
+
+                int j = i;
+                while (j > 0)
+                {
+                    if (a[j] < a[j - 1]) break;
+
+                    maxh += j % 2 == 0 ? (a[j - 1] - a[j]) : (a[j] - a[j - 1]);
+                    (a[j - 1], a[j]) = (a[j], a[j - 1]);
+                    j--;
+                }
+
+                if (maxh > h) break;
+                else ans++;
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
