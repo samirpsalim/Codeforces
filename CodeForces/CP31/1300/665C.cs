ba4aad784121abad04470825d81eabb912b94a82
Samir P Salim <samirpsalim.git@gmail.com>
2026-08-31 08:50:31 +0530

    CP31 sheet - 1300 rated - Q31

---
+++ CodeForces/CP31/1300/665C.cs
@@ -0,0 +1,26 @@
+﻿namespace Leetcode.CodeForces.CP31._1300
+{
+    internal class _665C
+    {
+        static void Solve(string[] args)
+        {
+            string s = Console.ReadLine()!;
+
+            char[] ans = new char[s.Length];
+
+            ans[0] = s[0];
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (ans[i - 1] != s[i]) ans[i] = s[i];
+                else if (i == s.Length - 1) ans[i] = s[i] == 'a' ? 'b' : 'a';
+                else if (ans[i-1] == s[i+1]) ans[i] = s[i] == 'a' ? 'b' : 'a';
+                else ans[i]= (s[i]!='a' && s[i+1]!='a') ?'a' 
+                            : (s[i] != 'b' && s[i + 1] != 'b') ? 'b'
+                            :'c';
+            }
+
+            Console.WriteLine(new string(ans));
+        }
+    }
+}
